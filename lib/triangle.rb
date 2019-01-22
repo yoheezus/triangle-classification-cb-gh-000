92b417518723e5de7c752eb9d040a1e42585c1f8
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-22 12:05:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,25 +2,18 @@ class Triangle
   # write code here
   attr_accessor :all_sides
 
-  def initialize(side_1, side_2, side_3)
-      @all_sides = []
-      @all_sides << side_1
-      @all_sides << side_2
-      @all_sides << side_3
+  def initialize(a, b, c)
+      @a = a
+      @b = b
+      @c = c
   end
 
   def kind
-      if @all_sides.include?(0) || (@all_sides[0] + @all_sides[1]) < @all_sides[2]
-              raise TriangleError
-      else
-          if @all_sides.uniq.length == 1
-              :equilateral
-          elsif @all_sides.uniq.length == 2
-              :isosceles
-          else
-              :scalene
-          end
-      end
+      
+  end
+
+  def validate_tri
+      valid = [(a + b > c), (a + c > b), (b + c > a)]
   end
 
 
